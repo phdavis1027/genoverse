d6ca51ffdc9a9ef1620cee4f086de9e42b8cd0db
mccannsa <mccannsa@appstate.edu>
2022-04-09 01:03:03 -0400

    beginning store work

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,61 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    trees: {
+      id: Number,
+      nodes: [
+        {
+          id: Number,
+          name: String,
+          dob: Date,
+          dod: Date,
+          pob: String,
+          gender: String,
+          nat: String,
+          misc: []
+        }
+      ],
+      edges: [
+        {
+          to: {
+            id: Number,
+            name: String,
+            dob: Date,
+            dod: Date,
+            pob: String,
+            gender: String,
+            nat: String,
+            misc: []
+          },
+          from: {
+            id: Number,
+            name: String,
+            dob: Date,
+            dod: Date,
+            pob: String,
+            gender: String,
+            nat: String,
+            misc: []
+          },
+          label: String,
+        }
+      ]
+    }
   },
   getters: {
+
   },
   mutations: {
+    addTree(state, tree) {
+      state.trees.push(tree)
+    },
+    addNode(state, tid, node) {
+      state.trees[tid].nodes.push(node)
+    },
+    addEdge(state, tid, edge) {
+      state.trees[tid].edges.push(edge)
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
+  // actions: {
+  // },
 })
